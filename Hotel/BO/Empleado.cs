5439cd175e8515bb6f99d883a1761b534d02f73e
Ricardo Canul <ricardocanul7@gmail.com>
2019-02-14 21:44:05 -0600

    Se agrega modelo Empleado y Usuario

---
+++ Hotel/BO/Empleado.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hotel.BO
+{
+    class Empleado
+    {
+        private string nombre;
+        private string apellido;
+        private string direccion;
+        private string telefono;
+        private string horario;
+
+        public string Nombre
+        {
+            get { return nombre; }
+            set { nombre = value; }
+        }
+
+        public string Apellido
+        {
+            get { return apellido; }
+            set { apellido = value; }
+        }
+
+        public string Direccion
+        {
+            get { return direccion; }
+            set { direccion = value; }
+        }
+
+        public string Telefono
+        {
+            get { return telefono; }
+            set { telefono = value; }
+        }
+
+        public string Horario
+        {
+            get { return horario; }
+            set { horario = value; }
+        }
+    }
+}
---
+++ Hotel/BO/UsuarioBO.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hotel.BO
+{
+    class UsuarioBO
+    {
+        protected string email;
+        protected string password;
+
+        public string Email
+        {
+            get { return email; }
+            set { email = value; }
+        }
+
+        public string Password
+        {
+            get { return password; }
+            set { password = value; }
+        }
+    }
+}
