d942310b20ab5215b37c7951130a3c94f2e67439
charlygoba <charlygoba@gmail.com>
2019-02-21 08:32:11 -0600

    Se agrego Form de habitaciones

---
+++ Hotel/GUI/frm_habitaciones.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Hotel.GUI
+{
+    public partial class frm_habitaciones : Form
+    {
+        public frm_habitaciones()
+        {
+            InitializeComponent();
+        }
+
+        private void frm_habitaciones_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
