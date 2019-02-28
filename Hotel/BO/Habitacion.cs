a39d3e3749319bee1cbed4998dd125951ffefb0a
Aaron Herrera <aaron.ssss62@gmail.com>
2019-02-28 11:54:01 -0600

    Agregue clase habitacion y atributos

---
+++ Hotel/BO/Habitacion.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hotel.BO
+{
+    class Habitacion
+    {
+        private int num_habitacion;
+        private string estado;
+
+        public int Num_habitacion
+        {
+            get { return num_habitacion; }
+            set { num_habitacion = value; }
+        }
+        public string Estado
+        {
+            get { return estado; }
+            set { estado = value; }
+        }
+
+
+    }
+}
