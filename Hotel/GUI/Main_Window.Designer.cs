df3c36d311affb1248b4b4538faa9c0a091427fc
Ricardo Canul <ricardocanul7@gmail.com>
2019-03-09 14:14:43 -0600

    Se cambio fuente de Titulo

--- Hotel/GUI/Main_Window.Designer.cs
+++ Hotel/GUI/Main_Window.Designer.cs
@@ -30,21 +30,21 @@
         {
             this.panel1 = new System.Windows.Forms.Panel();
             this.panel_izquierdo = new System.Windows.Forms.Panel();
+            this.btn_config = new System.Windows.Forms.Button();
+            this.btn_inicio = new System.Windows.Forms.Button();
+            this.panel_user_info = new System.Windows.Forms.Panel();
+            this.lbl_nombre_usuario = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btn_cerrar_sesion = new System.Windows.Forms.Button();
+            this.panel_logo = new System.Windows.Forms.Panel();
             this.panel_titulo = new System.Windows.Forms.Panel();
             this.lbl_titulo = new System.Windows.Forms.Label();
             this.panel_principal = new System.Windows.Forms.Panel();
-            this.btn_cerrar_sesion = new System.Windows.Forms.Button();
-            this.panel_user_info = new System.Windows.Forms.Panel();
-            this.btn_inicio = new System.Windows.Forms.Button();
-            this.btn_config = new System.Windows.Forms.Button();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.panel_logo = new System.Windows.Forms.Panel();
-            this.lbl_nombre_usuario = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
             this.panel_izquierdo.SuspendLayout();
-            this.panel_titulo.SuspendLayout();
             this.panel_user_info.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel_titulo.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
@@ -70,59 +70,20 @@
             this.panel_izquierdo.Size = new System.Drawing.Size(200, 529);
             this.panel_izquierdo.TabIndex = 1;
             // 
-            // panel_titulo
+            // btn_config
             // 
-            this.panel_titulo.BackColor = System.Drawing.Color.Silver;
-            this.panel_titulo.Controls.Add(this.lbl_titulo);
-            this.panel_titulo.Dock = System.Windows.Forms.DockStyle.Top;
-            this.panel_titulo.Location = new System.Drawing.Point(220, 60);
-            this.panel_titulo.Name = "panel_titulo";
-            this.panel_titulo.Size = new System.Drawing.Size(875, 54);
-            this.panel_titulo.TabIndex = 1;
-            // 
-            // lbl_titulo
-            // 
-            this.lbl_titulo.AutoSize = true;
-            this.lbl_titulo.Font = new System.Drawing.Font("Comic Sans MS", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lbl_titulo.Location = new System.Drawing.Point(22, 11);
-            this.lbl_titulo.Name = "lbl_titulo";
-            this.lbl_titulo.Size = new System.Drawing.Size(71, 29);
-            this.lbl_titulo.TabIndex = 0;
-            this.lbl_titulo.Text = "Titulo";
-            // 
-            // panel_principal
-            // 
-            this.panel_principal.BackColor = System.Drawing.Color.GhostWhite;
-            this.panel_principal.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panel_principal.Location = new System.Drawing.Point(220, 114);
-            this.panel_principal.Name = "panel_principal";
-            this.panel_principal.Size = new System.Drawing.Size(875, 529);
-            this.panel_principal.TabIndex = 2;
-            // 
-            // btn_cerrar_sesion
-            // 
-            this.btn_cerrar_sesion.BackColor = System.Drawing.Color.Crimson;
-            this.btn_cerrar_sesion.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.btn_cerrar_sesion.FlatAppearance.BorderSize = 0;
-            this.btn_cerrar_sesion.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btn_cerrar_sesion.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btn_cerrar_sesion.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.btn_cerrar_sesion.Location = new System.Drawing.Point(0, 488);
-            this.btn_cerrar_sesion.Name = "btn_cerrar_sesion";
-            this.btn_cerrar_sesion.Size = new System.Drawing.Size(200, 41);
-            this.btn_cerrar_sesion.TabIndex = 1;
-            this.btn_cerrar_sesion.Text = "CERRAR SESIÓN";
-            this.btn_cerrar_sesion.UseVisualStyleBackColor = false;
-            // 
-            // panel_user_info
-            // 
-            this.panel_user_info.Controls.Add(this.lbl_nombre_usuario);
-            this.panel_user_info.Controls.Add(this.pictureBox1);
-            this.panel_user_info.Dock = System.Windows.Forms.DockStyle.Top;
-            this.panel_user_info.Location = new System.Drawing.Point(0, 0);
-            this.panel_user_info.Name = "panel_user_info";
-            this.panel_user_info.Size = new System.Drawing.Size(200, 146);
-            this.panel_user_info.TabIndex = 2;
+            this.btn_config.BackColor = System.Drawing.Color.Black;
+            this.btn_config.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btn_config.FlatAppearance.BorderSize = 0;
+            this.btn_config.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_config.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_config.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btn_config.Location = new System.Drawing.Point(0, 187);
+            this.btn_config.Name = "btn_config";
+            this.btn_config.Size = new System.Drawing.Size(200, 41);
+            this.btn_config.TabIndex = 4;
+            this.btn_config.Text = "CONFIGURACIÓN";
+            this.btn_config.UseVisualStyleBackColor = false;
             // 
             // btn_inicio
             // 
@@ -139,20 +100,26 @@
             this.btn_inicio.Text = "INICIO";
             this.btn_inicio.UseVisualStyleBackColor = false;
             // 
-            // btn_config
+            // panel_user_info
             // 
-            this.btn_config.BackColor = System.Drawing.Color.Black;
-            this.btn_config.Dock = System.Windows.Forms.DockStyle.Top;
-            this.btn_config.FlatAppearance.BorderSize = 0;
-            this.btn_config.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btn_config.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btn_config.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.btn_config.Location = new System.Drawing.Point(0, 187);
-            this.btn_config.Name = "btn_config";
-            this.btn_config.Size = new System.Drawing.Size(200, 41);
-            this.btn_config.TabIndex = 4;
-            this.btn_config.Text = "CONFIGURACIÓN";
-            this.btn_config.UseVisualStyleBackColor = false;
+            this.panel_user_info.Controls.Add(this.lbl_nombre_usuario);
+            this.panel_user_info.Controls.Add(this.pictureBox1);
+            this.panel_user_info.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel_user_info.Location = new System.Drawing.Point(0, 0);
+            this.panel_user_info.Name = "panel_user_info";
+            this.panel_user_info.Size = new System.Drawing.Size(200, 146);
+            this.panel_user_info.TabIndex = 2;
+            // 
+            // lbl_nombre_usuario
+            // 
+            this.lbl_nombre_usuario.AutoSize = true;
+            this.lbl_nombre_usuario.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_nombre_usuario.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.lbl_nombre_usuario.Location = new System.Drawing.Point(40, 92);
+            this.lbl_nombre_usuario.Name = "lbl_nombre_usuario";
+            this.lbl_nombre_usuario.Size = new System.Drawing.Size(116, 25);
+            this.lbl_nombre_usuario.TabIndex = 1;
+            this.lbl_nombre_usuario.Text = "Juanito L.";
             // 
             // pictureBox1
             // 
@@ -165,6 +132,21 @@
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
+            // btn_cerrar_sesion
+            // 
+            this.btn_cerrar_sesion.BackColor = System.Drawing.Color.Crimson;
+            this.btn_cerrar_sesion.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.btn_cerrar_sesion.FlatAppearance.BorderSize = 0;
+            this.btn_cerrar_sesion.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_cerrar_sesion.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_cerrar_sesion.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btn_cerrar_sesion.Location = new System.Drawing.Point(0, 488);
+            this.btn_cerrar_sesion.Name = "btn_cerrar_sesion";
+            this.btn_cerrar_sesion.Size = new System.Drawing.Size(200, 41);
+            this.btn_cerrar_sesion.TabIndex = 1;
+            this.btn_cerrar_sesion.Text = "CERRAR SESIÓN";
+            this.btn_cerrar_sesion.UseVisualStyleBackColor = false;
+            // 
             // panel_logo
             // 
             this.panel_logo.BackColor = System.Drawing.Color.Silver;
@@ -176,16 +158,34 @@
             this.panel_logo.Size = new System.Drawing.Size(200, 54);
             this.panel_logo.TabIndex = 0;
             // 
-            // lbl_nombre_usuario
+            // panel_titulo
             // 
-            this.lbl_nombre_usuario.AutoSize = true;
-            this.lbl_nombre_usuario.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lbl_nombre_usuario.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.lbl_nombre_usuario.Location = new System.Drawing.Point(40, 92);
-            this.lbl_nombre_usuario.Name = "lbl_nombre_usuario";
-            this.lbl_nombre_usuario.Size = new System.Drawing.Size(116, 25);
-            this.lbl_nombre_usuario.TabIndex = 1;
-            this.lbl_nombre_usuario.Text = "Juanito L.";
+            this.panel_titulo.BackColor = System.Drawing.Color.Silver;
+            this.panel_titulo.Controls.Add(this.lbl_titulo);
+            this.panel_titulo.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel_titulo.Location = new System.Drawing.Point(220, 60);
+            this.panel_titulo.Name = "panel_titulo";
+            this.panel_titulo.Size = new System.Drawing.Size(875, 54);
+            this.panel_titulo.TabIndex = 1;
+            // 
+            // lbl_titulo
+            // 
+            this.lbl_titulo.AutoSize = true;
+            this.lbl_titulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_titulo.Location = new System.Drawing.Point(22, 11);
+            this.lbl_titulo.Name = "lbl_titulo";
+            this.lbl_titulo.Size = new System.Drawing.Size(65, 25);
+            this.lbl_titulo.TabIndex = 0;
+            this.lbl_titulo.Text = "Titulo";
+            // 
+            // panel_principal
+            // 
+            this.panel_principal.BackColor = System.Drawing.Color.GhostWhite;
+            this.panel_principal.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel_principal.Location = new System.Drawing.Point(220, 114);
+            this.panel_principal.Name = "panel_principal";
+            this.panel_principal.Size = new System.Drawing.Size(875, 529);
+            this.panel_principal.TabIndex = 2;
             // 
             // Main_Window
             // 
@@ -199,11 +199,11 @@
             this.Text = "Sistema de gestión de habitaciones";
             this.panel1.ResumeLayout(false);
             this.panel_izquierdo.ResumeLayout(false);
-            this.panel_titulo.ResumeLayout(false);
-            this.panel_titulo.PerformLayout();
             this.panel_user_info.ResumeLayout(false);
             this.panel_user_info.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel_titulo.ResumeLayout(false);
+            this.panel_titulo.PerformLayout();
             this.ResumeLayout(false);
 
         }
