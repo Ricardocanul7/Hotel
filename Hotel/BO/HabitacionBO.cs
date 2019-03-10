455aaa728a332d24e52c743509c96e689f62da44
Alan Moreno <alanalexismartinez@gmail.com>
2019-03-10 14:28:01 -0600

    Cambie el (name) de todos los elementos de contrnido pues eran muy largos. Quite el campo de número de habitaciones pues era irrelevante. Cambie y quité propiedades en la clase HabitacioBO e hice los eventos Click para cada boton.

--- Hotel/BO/HabitacionBO.cs
+++ Hotel/BO/HabitacionBO.cs
@@ -8,20 +8,61 @@ namespace Hotel.BO
 {
     class HabitacionBO
     {
-        private int num_habitacion;
-        private string estado;
+        private string nombre_hab;
+        private int numx_per;
+        private string tipo_hab;
+        private double precioN;
+        private double precioPA;
+        private double precioTA;
+        private double precioPATA;
+        private string descripHab;
 
-        public int Num_habitacion
+        public string Nombre_hab
         {
-            get { return num_habitacion; }
-            set { num_habitacion = value; }
+            get { return nombre_hab; }
+            set { nombre_hab = value; }
         }
-        public string Estado
+
+        public int Numx_per
         {
-            get { return estado; }
-            set { estado = value; }
+            get { return numx_per; }
+            set { numx_per = value; }
         }
 
+        public string Tipo_hab
+        {
+            get { return tipo_hab; }
+            set { tipo_hab = value; }
+        }
 
+        public double PrecioN
+        {
+            get { return precioN; }
+            set { precioN = value; }
+        }
+
+        public double PrecioPA
+        {
+            get { return precioPA; }
+            set { precioPA = value; }
+        }
+
+        public double PrecioTA
+        {
+            get { return precioTA; }
+            set { precioTA = value; }
+        }
+
+        public double PrecioPATA
+        {
+            get { return precioPATA; }
+            set { precioPATA = value; }
+        }
+
+        public string DescripHab
+        {
+            get { return descripHab; }
+            set { descripHab = value; }
+        }
     }
 }
--- Hotel/GUI/frm_habitaciones.cs
+++ Hotel/GUI/frm_habitaciones.cs
@@ -7,14 +7,52 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Hotel.BO;
 
 namespace Hotel.GUI
 {
     public partial class frm_habitaciones : Form
     {
+        HabitacionBO habitacionBO;
         public frm_habitaciones()
         {
             InitializeComponent();
+            habitacionBO = new HabitacionBO();
+
+            cbo_tipohab.Items.Add("SELECCIONAR");
+            cbo_tipohab.Items.Add("CABAÑA");
+            cbo_tipohab.Items.Add("CABAÑA ST");
+            cbo_tipohab.Items.Add("SUITE JAR");
+            cbo_tipohab.Items.Add("SUITE LAG");
+            cbo_tipohab.Items.Add("DUPLEX");
+            cbo_tipohab.Items.Add("DUPLEX LAG");
+
+            cbo_tipohab.SelectedIndex = 0;
+
+        }
+
+        private void btn_hab_guardar_Click(object sender, EventArgs e)
+        {
+            habitacionBO.Nombre_hab = txt_nombrehab.Text;
+            habitacionBO.Numx_per = Convert.ToInt32(txt_numx_per.Text);
+            habitacionBO.Tipo_hab = cbo_tipohab.Text;
+            habitacionBO.PrecioN = Convert.ToDouble(txt_precioN.Text);
+            habitacionBO.PrecioPA = Convert.ToDouble(txt_precioPA.Text);
+            habitacionBO.PrecioTA = Convert.ToDouble(txt_precioTA.Text);
+            habitacionBO.PrecioPATA = Convert.ToDouble(txt_precioPATA.Text);
+            habitacionBO.DescripHab = txt_descripHab.Text;
+        }
+
+        private void btn_hab_limpiar_Click(object sender, EventArgs e)
+        {
+            txt_nombrehab.Clear();
+            txt_numx_per.Clear();
+            cbo_tipohab.SelectedIndex = 0;
+            txt_precioN.Clear();
+            txt_precioPA.Clear();
+            txt_precioTA.Clear();
+            txt_precioPATA.Clear();
+            txt_descripHab.Clear();
         }
     }
 }
