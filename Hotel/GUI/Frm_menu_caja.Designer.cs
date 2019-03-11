43f9dcd184b74a93350f0c4d551cb9e78e92b0cd
charlygoba <charlygoba@gmail.com>
2019-03-11 10:29:26 -0600

    Se cambiaron los nombres de los botones de caja

--- Hotel/GUI/Frm_menu_caja.Designer.cs
+++ Hotel/GUI/Frm_menu_caja.Designer.cs
@@ -34,9 +34,9 @@
             this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
             this.toolStripSeparator3 = new System.Windows.Forms.ToolStripSeparator();
             this.btn_nvatransaccion = new System.Windows.Forms.ToolStripButton();
-            this.btn_consultar_res = new System.Windows.Forms.ToolStripButton();
-            this.btn_checkin = new System.Windows.Forms.ToolStripButton();
-            this.btn_chekout = new System.Windows.Forms.ToolStripButton();
+            this.brn_abrir_caja = new System.Windows.Forms.ToolStripButton();
+            this.btn_cerrar_caja = new System.Windows.Forms.ToolStripButton();
+            this.btn_reporte_caja = new System.Windows.Forms.ToolStripButton();
             this.metroLabel1 = new MetroFramework.Controls.MetroLabel();
             ((System.ComponentModel.ISupportInitialize)(this.Grd_Reservaciones)).BeginInit();
             this.btn_habitaciones.SuspendLayout();
@@ -58,11 +58,11 @@
             this.btn_habitaciones.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.btn_nvatransaccion,
             this.toolStripSeparator1,
-            this.btn_consultar_res,
+            this.brn_abrir_caja,
             this.toolStripSeparator2,
-            this.btn_checkin,
+            this.btn_cerrar_caja,
             this.toolStripSeparator3,
-            this.btn_chekout});
+            this.btn_reporte_caja});
             this.btn_habitaciones.Location = new System.Drawing.Point(0, 0);
             this.btn_habitaciones.Name = "btn_habitaciones";
             this.btn_habitaciones.RenderMode = System.Windows.Forms.ToolStripRenderMode.Professional;
@@ -95,30 +95,30 @@
             this.btn_nvatransaccion.Size = new System.Drawing.Size(135, 22);
             this.btn_nvatransaccion.Text = "Agregar Transacción";
             // 
-            // btn_consultar_res
+            // brn_abrir_caja
             // 
-            this.btn_consultar_res.Image = global::Hotel.Properties.Resources._023_ladder;
-            this.btn_consultar_res.ImageTransparentColor = System.Drawing.Color.Magenta;
-            this.btn_consultar_res.Name = "btn_consultar_res";
-            this.btn_consultar_res.Size = new System.Drawing.Size(79, 22);
-            this.btn_consultar_res.Text = "Abrir Caja";
+            this.brn_abrir_caja.Image = global::Hotel.Properties.Resources._023_ladder;
+            this.brn_abrir_caja.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.brn_abrir_caja.Name = "brn_abrir_caja";
+            this.brn_abrir_caja.Size = new System.Drawing.Size(79, 22);
+            this.brn_abrir_caja.Text = "Abrir Caja";
             // 
-            // btn_checkin
+            // btn_cerrar_caja
             // 
-            this.btn_checkin.Image = global::Hotel.Properties.Resources._024_safety_box1;
-            this.btn_checkin.ImageTransparentColor = System.Drawing.Color.Magenta;
-            this.btn_checkin.Name = "btn_checkin";
-            this.btn_checkin.Size = new System.Drawing.Size(85, 22);
-            this.btn_checkin.Text = "Cerrar Caja";
-            this.btn_checkin.ToolTipText = "Cerrar Caja";
+            this.btn_cerrar_caja.Image = global::Hotel.Properties.Resources._024_safety_box1;
+            this.btn_cerrar_caja.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btn_cerrar_caja.Name = "btn_cerrar_caja";
+            this.btn_cerrar_caja.Size = new System.Drawing.Size(85, 22);
+            this.btn_cerrar_caja.Text = "Cerrar Caja";
+            this.btn_cerrar_caja.ToolTipText = "Cerrar Caja";
             // 
-            // btn_chekout
+            // btn_reporte_caja
             // 
-            this.btn_chekout.Image = global::Hotel.Properties.Resources._049_notes1;
-            this.btn_chekout.ImageTransparentColor = System.Drawing.Color.Magenta;
-            this.btn_chekout.Name = "btn_chekout";
-            this.btn_chekout.Size = new System.Drawing.Size(159, 22);
-            this.btn_chekout.Text = "Reporte de transacciones";
+            this.btn_reporte_caja.Image = global::Hotel.Properties.Resources._049_notes1;
+            this.btn_reporte_caja.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btn_reporte_caja.Name = "btn_reporte_caja";
+            this.btn_reporte_caja.Size = new System.Drawing.Size(159, 22);
+            this.btn_reporte_caja.Text = "Reporte de transacciones";
             // 
             // metroLabel1
             // 
@@ -153,11 +153,11 @@
         private System.Windows.Forms.ToolStrip btn_habitaciones;
         private System.Windows.Forms.ToolStripButton btn_nvatransaccion;
         private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
-        private System.Windows.Forms.ToolStripButton btn_consultar_res;
+        private System.Windows.Forms.ToolStripButton brn_abrir_caja;
         private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
-        private System.Windows.Forms.ToolStripButton btn_checkin;
+        private System.Windows.Forms.ToolStripButton btn_cerrar_caja;
         private System.Windows.Forms.ToolStripSeparator toolStripSeparator3;
-        private System.Windows.Forms.ToolStripButton btn_chekout;
+        private System.Windows.Forms.ToolStripButton btn_reporte_caja;
         private MetroFramework.Controls.MetroLabel metroLabel1;
     }
 }
