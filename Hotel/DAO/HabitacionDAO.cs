da116be7693d16eba0c3a1a89556df591ac0f415
Alan Moreno <alanalexismartinez@gmail.com>
2019-03-14 08:19:48 -0600

    Agregué el método de agregar para el formulario de habitaciones.

--- Hotel/DAO/HabitacionDAO.cs
+++ Hotel/DAO/HabitacionDAO.cs
@@ -3,19 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Hotel.BO;
+using System.Data;
 
 namespace Hotel.DAO
 {
     class HabitacionDAO
     {
-        /*  -Agregar
-           -Modificar
-           -Eliminar
-           -Buscar  */
 
-        public bool Agregar()
+        Conexion conexion;
+
+        public HabitacionDAO()
         {
-            return true;
+            conexion = new Conexion();
+        }
+
+        public int Agregar(HabitacionBO habitacionBO)
+        {
+            string ComandoSQL = string.Format("INSERT INTO  habitacion(nombre, max_ninios, max_adultos, precio_alta, precio_baja, " +
+                "precio_adic_alta, precio_adic_baja, tipo, descripcion, estado)VALUES('{0}', '{1}', '{2}', '{3}', '{4}', '{5}', '{6}', " +
+                "'{7}', '{8}', '{9}');"
+                , habitacionBO.Nombre_hab, habitacionBO.Max_ninios, habitacionBO.Max_adultos, 
+                habitacionBO.PrecioTA, habitacionBO.PrecioN, habitacionBO.PrecioPATA, habitacionBO.PrecioPA, habitacionBO.Tipo_hab,
+                habitacionBO.DescripHab, 'D');
+            return conexion.EjecutarComando(ComandoSQL);
         }
 
         public bool Modificar()
