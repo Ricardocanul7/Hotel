d14e8554572d96fd8cb267cfcfda6eec4e4ac471
Ricardo Canul <ricardocanul7@gmail.com>
2019-03-14 00:09:55 -0600

    Se crea y se verifica el funcionamiento de agregar en reservas

--- Hotel/DAO/ReservaDAO.cs
+++ Hotel/DAO/ReservaDAO.cs
@@ -9,11 +9,11 @@ namespace Hotel.DAO
 {
     class ReservaDAO
     {
-        Conexion conn;
+        Conexion conn = new Conexion();
+
         public int Agregar(ReservasBO reserva)
         {
-            string commandSQL = String.Format("INSERT INTO reservacion(num_habitacion, fecha_entrada, fecha_salida, detalles, cliente)" +
-                                               "VALUES('{0}','{1}','{2}','{3}','{4}','{5}')", reserva.Habitacion);
+            string commandSQL = String.Format("INSERT INTO reservacion(num_habitacion, fecha_entrada, fecha_salida, detalles, cliente) VALUES('{0}', '{1}', '{2}', '{3}', '{4}');", reserva.Habitacion.Num_habitacion, reserva.Fecha_entrada.ToString("yyyy-MM-dd H:mm:ss"), reserva.Fecha_salida.ToString("yyyy-MM-dd H:mm:ss"), reserva.Detalles, reserva.Cliente.Id_cliente);
             return conn.EjecutarComando(commandSQL);
         }
     }
