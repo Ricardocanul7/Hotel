28b39e33b6143ee88dc7ad56a15886277c6c5766
Alan Moreno <alanalexismartinez@gmail.com>
2019-03-13 22:12:43 -0600

    agregué atributos y propiedades(num_hab, empleadoBO)

--- Hotel/BO/HabitacionBO.cs
+++ Hotel/BO/HabitacionBO.cs
@@ -8,15 +8,23 @@ namespace Hotel.BO
 {
     class HabitacionBO
     {
+        private int num_habitacion;
         private string nombre_hab;
         private int max_adultos;
         private int max_ninios;
         private string tipo_hab;
         private double precioN;
-        private double precioPA;
-        private double precioTA;
-        private double precioPATA;
+        private double precioPA; //PA=persona adicional
+        private double precioTA; //TA=temporada alta
+        private double precioPATA; //PATA=persona adicional temporada alta
         private string descripHab;
+        private EmpleadoBO empleadoBO;
+
+        public int Num_habitacion
+        {
+            get { return num_habitacion; }
+            set { num_habitacion = value; }
+        }
 
         public string Nombre_hab
         {
@@ -70,6 +78,12 @@ namespace Hotel.BO
         {
             get { return descripHab; }
             set { descripHab = value; }
+        }
+
+        public EmpleadoBO EmpleadoBO
+        {
+            get { return empleadoBO; }
+            set { empleadoBO = value; }
         }
     }
 }
