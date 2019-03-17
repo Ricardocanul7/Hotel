c5d08ce04304e2ff8024bfb88eccec858c8cd311
Ricardo Canul <ricardocanul7@gmail.com>
2019-03-16 19:40:50 -0600

    Se cambia formato de comandoSQL para mas facilidad de cambios posteriores

--- Hotel/DAO/ReservaDAO.cs
+++ Hotel/DAO/ReservaDAO.cs
@@ -13,7 +13,15 @@ namespace Hotel.DAO
 
         public int Agregar(ReservasBO reserva)
         {
-            string commandSQL = String.Format("INSERT INTO reservacion(num_habitacion, fecha_entrada, fecha_salida, detalles, cliente) VALUES('{0}', '{1}', '{2}', '{3}', '{4}');", reserva.Habitacion.Num_habitacion, reserva.Fecha_entrada.ToString("yyyy-MM-dd H:mm:ss"), reserva.Fecha_salida.ToString("yyyy-MM-dd H:mm:ss"), reserva.Detalles, reserva.Cliente.Cliente_id);
+            string col1 = "num_habitacion";
+            string col2 = "fecha_entrada";
+            string col3 = "fecha_salida";
+            string col4 = "detalles";
+            string col5 = "cliente";
+
+            string commandCol = String.Format("INSERT INTO reservacion({0}, {1}, {2}, {3}, {4})", col1, col2, col3, col4, col5);
+            string commandValues = String.Format(" VALUES('{0}', '{1}', '{2}', '{3}', '{4}');", reserva.Habitacion.Num_habitacion, reserva.Fecha_entrada.ToString("yyyy-MM-dd H:mm:ss"), reserva.Fecha_salida.ToString("yyyy-MM-dd H:mm:ss"), reserva.Detalles, reserva.Cliente.Cliente_id);
+            string commandSQL = commandCol + commandValues;
             return conn.EjecutarComando(commandSQL);
         }
     }
