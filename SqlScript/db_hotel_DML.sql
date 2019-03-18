223162ea6e2a4baa423890ae5e3f84eb52b3f7ea
Ricardo Canul <ricardocanul7@gmail.com>
2019-03-18 11:56:06 -0600

    Se agrega script DML

---
+++ SqlScript/db_hotel_DML.sql
@@ -0,0 +1,24 @@
+use db_hotel;
+
+/* Estado de habitacion */
+INSERT INTO estado_habitacion (estado) VALUES ('No disponible');
+INSERT INTO estado_habitacion (estado) VALUES ('Mantenimiento');
+INSERT INTO estado_habitacion (estado) VALUES ('Limpieza');
+INSERT INTO estado_habitacion (estado) VALUES ('Disponible');
+
+/* Puesto de empleado */
+INSERT INTO puesto_empleado (puesto) VALUES ('Recepcion');
+INSERT INTO puesto_empleado (puesto) VALUES ('Limpieza');
+
+/* Tipo de transaccion */
+INSERT INTO tipo_transaccion (nombre_transaccion) VALUES ('Efectivo');
+INSERT INTO tipo_transaccion (nombre_transaccion) VALUES ('Tarjeta');
+
+/* Tipo de usuario */
+INSERT INTO tipo_usuario (nombre) VALUES ('Admin');
+INSERT INTO tipo_usuario (nombre) VALUES ('Recepcionista');
+INSERT INTO tipo_usuario (nombre) VALUES ('Propietario');
+
+/* Tipo de habitaciones */
+INSERT INTO tipo_habitacion (nombre_tipo) VALUES ('Sencilla');
+INSERT INTO tipo_habitacion (nombre_tipo) VALUES ('Doble');
