683fbe3da0f62c9d498c872f817ba2515dbf96ad
Ricardo Canul <ricardocanul7@gmail.com>
2019-03-18 12:09:50 -0600

    Se corrigen campos de sentencias en habitacionDAO

--- Hotel/DAO/HabitacionDAO.cs
+++ Hotel/DAO/HabitacionDAO.cs
@@ -21,7 +21,7 @@ namespace Hotel.DAO
         public int Agregar(HabitacionBO habitacionBO)
         {
             string ComandoSQL = string.Format("INSERT INTO  habitacion(nombre, max_ninios, max_adultos, precio_alta, precio_baja, " +
-                "precio_adic_alta, precio_adic_baja, tipo, descripcion, estado)VALUES('{0}', '{1}', '{2}', '{3}', '{4}', '{5}', '{6}', " +
+                "precio_adic_alta, precio_adic_baja, tipo_hab, descripcion, estado_hab)VALUES('{0}', '{1}', '{2}', '{3}', '{4}', '{5}', '{6}', " +
                 "'{7}', '{8}', '{9}');"
                 , habitacionBO.Nombre_hab, habitacionBO.Max_ninios, habitacionBO.Max_adultos, 
                 habitacionBO.PrecioTA, habitacionBO.PrecioN, habitacionBO.PrecioPATA, habitacionBO.PrecioPA, habitacionBO.Tipo_hab,
@@ -32,7 +32,7 @@ namespace Hotel.DAO
         public int Modificar(HabitacionBO habitacionBO)
         {
             string ComandoSQL = string.Format("UPDATE habitacion SET nombre='{1}', max_ninios='{2}', max_adultos='{3}', precio_alta='{4}', " +
-                "precio_baja='{5}', precio_adic_alta='{6}', precio_adic_baja='{7}', tipo='{8}', descripcion='{9}', estado='{10}'  WHERE num_habitacion={0}",
+                "precio_baja='{5}', precio_adic_alta='{6}', precio_adic_baja='{7}', tipo_hab='{8}', descripcion='{9}', estado_hab='{10}'  WHERE num_habitacion={0}",
                 habitacionBO.Num_habitacion, habitacionBO.Nombre_hab, habitacionBO.Max_ninios, habitacionBO.Max_adultos, habitacionBO.PrecioTA,
                 habitacionBO.PrecioN, habitacionBO.PrecioPATA, habitacionBO.PrecioPA, habitacionBO.Tipo_hab, habitacionBO.DescripHab, habitacionBO.Estado_habitacion);
             return conexion.EjecutarComando(ComandoSQL);
