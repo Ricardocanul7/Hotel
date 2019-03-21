6febc12d70b87e2db367b4f377c004b611062e6d
charlygoba <charlygoba@gmail.com>
2019-03-21 03:26:57 -0600

    Se agrego Form Corte de Caja

---
+++ Hotel/GUI/Frm_corte_caja.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Hotel.GUI
+{
+    public partial class Frm_corte_caja : MetroFramework.Forms.MetroForm
+    {
+        public Frm_corte_caja()
+        {
+            InitializeComponent();
+        }
+
+     
+    }
+}
