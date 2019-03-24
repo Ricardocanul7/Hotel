6c4cd144d89d95b357398fabffc9d62bdd096b2e
Alan Moreno <alanalexismartinez@gmail.com>
2019-03-24 00:14:27 -0600

    Agregué métodos crud en clases tipo y estado de habitación en capa DAO

---
+++ Hotel/DAO/EstadohabDAO.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Hotel.BO;
+
+namespace Hotel.DAO
+{
+    class EstadohabDAO
+    {
+        Conexion conexion;
+        public EstadohabDAO()
+        {
+            conexion = new Conexion();
+        }
+
+        public int Agregar(EstadohabBO estadohabBO)
+        {
+            return 0;
+        }
+
+        public int Modificar(EstadohabBO estadohabBO)
+        {
+            return 0;
+        }
+
+        public int Eliminar(EstadohabBO estadohabBO)
+        {
+            return 0;
+        }
+
+        public int Buscar()
+        {
+            return 0;
+        }
+    }
+}
---
+++ Hotel/DAO/TipohabDAO.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Hotel.BO;
+
+namespace Hotel.DAO
+{
+    class TipohabDAO
+    {
+        Conexion conexion;
+        public TipohabDAO()
+        {
+            conexion = new Conexion();
+        }
+
+        public int Agregar(TipohabBO tipohabBO)
+        {
+            return 0;
+        }
+
+        public int Modificar(TipohabBO tipohabBO)
+        {
+            return 0;
+        }
+
+        public int Eliminar(TipohabBO tipohabBO)
+        {
+            return 0;
+        }
+
+        public int Modificar()
+        {
+            return 0;
+        }
+    }
+}
