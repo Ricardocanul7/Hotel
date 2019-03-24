b0afa09d3c32dc2d4894747c6a406e92d048ba41
Alan Moreno <alanalexismartinez@gmail.com>
2019-03-23 19:21:08 -0600

    Agregué clases Tipo y Estado habitación en la capa BO, cada clase con atributos y propiedades

---
+++ Hotel/BO/EstadohabBO.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hotel.BO
+{
+    class EstadohabBO
+    {
+        private int estado_id;
+        private string nombre;
+
+        public int Estado_id
+        {
+            get { return estado_id; }
+            set { estado_id = value; }
+        }
+
+        public string Nombre
+        {
+            get { return nombre; }
+            set { nombre = value; }
+        }
+    }
+}
---
+++ Hotel/BO/TipohabBO.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hotel.BO
+{
+    class TipohabBO
+    {
+        private int tipo_id;
+        private string nombre;
+
+        public int Tipo_id
+        {
+            get { return tipo_id; }
+            set { tipo_id = value; }
+        }
+
+        public string Nombre
+        {
+            get { return nombre; }
+            set { nombre = value; }
+        }
+    }
+}
