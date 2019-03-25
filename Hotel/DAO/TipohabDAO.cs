2299ced388b8b1863c66efbbeb82fa0e9fa7f97e
Alan Moreno <alanalexismartinez@gmail.com>
2019-03-25 10:35:54 -0600

    Realicé el CRUD.

--- Hotel/DAO/TipohabDAO.cs
+++ Hotel/DAO/TipohabDAO.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Hotel.BO;
+using System.Data;
 
 namespace Hotel.DAO
 {
@@ -17,22 +18,26 @@ namespace Hotel.DAO
 
         public int Agregar(TipohabBO tipohabBO)
         {
-            return 0;
+            string ComandoSQL = string.Format("INSERT INTO  tipo_habitacion(nombre_tipo)VALUES('{0}');",tipohabBO.Nombre);
+            return conexion.EjecutarComando(ComandoSQL);
         }
 
         public int Modificar(TipohabBO tipohabBO)
         {
-            return 0;
+            string ComandoSQL = string.Format("UPDATE tipo_habitacion SET nombre_tipo='{1}' WHERE tipo_id={0}", tipohabBO.Tipo_id, tipohabBO.Nombre);
+            return conexion.EjecutarComando(ComandoSQL);
         }
 
         public int Eliminar(TipohabBO tipohabBO)
         {
-            return 0;
+            string ComandoSQL = string.Format("DELETE FROM tipo_habitacion WHERE tipo_id={0}", tipohabBO.Tipo_id);
+            return conexion.EjecutarComando(ComandoSQL);
         }
 
-        public int Modificar()
+        public DataTable Buscar()
         {
-            return 0;
+            string commandSQL = "SELECT * from tipo_habitacion";
+            return conexion.EjecutarSentencia(commandSQL);
         }
     }
 }
