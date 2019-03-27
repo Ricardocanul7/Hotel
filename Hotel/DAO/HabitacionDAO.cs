3491aba1596c332c0252b761bf1132655d8672d7
Alan Moreno <alanalexismartinez@gmail.com>
2019-03-27 14:15:14 -0600

    Corregí sentencias para agregar, modificar y buscar.

--- Hotel/DAO/HabitacionDAO.cs
+++ Hotel/DAO/HabitacionDAO.cs
@@ -20,21 +20,20 @@ namespace Hotel.DAO
 
         public int Agregar(HabitacionBO habitacionBO)
         {
-            string ComandoSQL = string.Format("INSERT INTO  habitacion(nombre, max_ninios, max_adultos, precio_alta, precio_baja, " +
-                "precio_adic_alta, precio_adic_baja, tipo_hab, descripcion, estado_hab)VALUES('{0}', '{1}', '{2}', '{3}', '{4}', '{5}', '{6}', " +
-                "'{7}', '{8}', '{9}');"
+            string ComandoSQL = string.Format("INSERT INTO  habitacion(nombre, max_ninios, max_adultos, precio_baja, " +
+                "tipo_hab, descripcion, estado_hab)VALUES('{0}', '{1}', '{2}', '{3}', '{4}', '{5}', '{6}');"
                 , habitacionBO.Nombre_hab, habitacionBO.Max_ninios, habitacionBO.Max_adultos, 
-                habitacionBO.PrecioTA, habitacionBO.PrecioN, habitacionBO.PrecioPATA, habitacionBO.PrecioPA, habitacionBO.Tipo.Tipo_id,
+                habitacionBO.PrecioN, habitacionBO.Tipo.Tipo_id,
                 habitacionBO.DescripHab, habitacionBO.Estado.Estado_id);
             return conexion.EjecutarComando(ComandoSQL);
         }
 
         public int Modificar(HabitacionBO habitacionBO)
         {
-            string ComandoSQL = string.Format("UPDATE habitacion SET nombre='{1}', max_ninios='{2}', max_adultos='{3}', precio_alta='{4}', " +
-                "precio_baja='{5}', precio_adic_alta='{6}', precio_adic_baja='{7}', tipo_hab='{8}', descripcion='{9}', estado_hab='{10}'  WHERE num_habitacion={0}",
-                habitacionBO.Num_habitacion, habitacionBO.Nombre_hab, habitacionBO.Max_ninios, habitacionBO.Max_adultos, habitacionBO.PrecioTA,
-                habitacionBO.PrecioN, habitacionBO.PrecioPATA, habitacionBO.PrecioPA, habitacionBO.Tipo.Tipo_id, habitacionBO.DescripHab, habitacionBO.Estado.Estado_id);
+            string ComandoSQL = string.Format("UPDATE habitacion SET nombre='{1}', max_ninios='{2}', max_adultos='{3}', " +
+                "precio_baja='{4}', tipo_hab='{5}', descripcion='{6}', estado_hab='{7}'  WHERE num_habitacion={0}",
+                habitacionBO.Num_habitacion, habitacionBO.Nombre_hab, habitacionBO.Max_ninios, habitacionBO.Max_adultos,
+                habitacionBO.PrecioN, habitacionBO.Tipo.Tipo_id, habitacionBO.DescripHab, habitacionBO.Estado.Estado_id);
             return conexion.EjecutarComando(ComandoSQL);
         }
 
@@ -52,16 +51,13 @@ namespace Hotel.DAO
             string col1 = "nombre";
             string col2 = "max_ninios";
             string col3 = "max_adultos";
-            string col4 = "precio_alta";
-            string col5 = "precio_baja";
-            string col6 = "precio_adic_alta";
-            string col7 = "precio_adic_baja";
-            string col8 = "nombre_tipo";
-            string col9 = "descripcion";
-            string col10 = "estado";
+            string col4 = "precio_baja";
+            string col5 = "nombre_tipo";
+            string col6 = "descripcion";
+            string col7 = "estado";
 
-            string selectSQL = String.Format("SELECT {0}, {1}, {2}, {3}, {4}, {5}, {6}, {7}, {8}, {9}, {10} ", col0, col1, col2, col3, col4, col5, col6, col7, col8, col9, col10);
-            string fromSQL = "FROM habitacion as a, tipo_habitacion as b, estado_habitacion as c where a.tipo_hab=b.tipo_id and a.estado_hab=c.estado_id";
+            string selectSQL = String.Format("SELECT {0}, {1}, {2}, {3}, {4}, {5}, {6}, {7} ", col0, col1, col2, col3, col4,  col5, col6, col7);
+            string fromSQL = "FROM habitacion as a, tipo_habitacion as b, estado_habitacion as c WHERE a.tipo_hab=b.tipo_id and a.estado_hab=c.estado_id";
             string commandSQL = selectSQL + fromSQL;
             return conexion.EjecutarSentencia(commandSQL);
         }
@@ -95,10 +91,7 @@ namespace Hotel.DAO
                 habitacionBO.Nombre_hab = row[0]["nombre"].ToString();
                 habitacionBO.Max_ninios = Convert.ToInt32(row[0]["max_ninios"]);
                 habitacionBO.Max_adultos = Convert.ToInt32(row[0]["max_adultos"]);
-                habitacionBO.PrecioTA = Convert.ToDouble(row[0]["precio_alta"]);
                 habitacionBO.PrecioN = Convert.ToDouble(row[0]["precio_baja"]);
-                habitacionBO.PrecioPATA = Convert.ToDouble(row[0]["precio_adic_alta"]);
-                habitacionBO.PrecioPA = Convert.ToDouble(row[0]["precio_adic_baja"]);
                 habitacionBO.Tipo.Nombre = row[0]["tipo_hab"].ToString();
                 habitacionBO.DescripHab = row[0]["descripcion"].ToString();
                 habitacionBO.Estado.Nombre = row[0]["estado_hab"].ToString();
