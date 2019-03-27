88335031348f26316233c232ce7900868511f5dd
charlygoba <charlygoba@gmail.com>
2019-03-27 17:05:08 -0600

    Se preparo DAO y BO Transaccion

---
+++ Hotel/DAO/TransaccionDAO.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Hotel.BO;
+
+namespace Hotel.DAO
+{
+    class TransaccionDAO
+    {
+        Conexion Miconexion;
+        public TransaccionDAO()
+        {
+            Miconexion = new Conexion();
+        }
+
+        public int Agregar(TransaccionBO transaccionbo)
+        {
+            string ComandoSQL = string.Format("INSERT INTO corte_caja (corte_id, monto, fecha, hora, usuario_id)VALUES('{0}','{1}','{2}','{3}','1');", cajabo.Corte_id, cajabo.Monto, cajabo.Fecha.ToString("yyyy-MM-dd"), cajabo.Hora.ToString("HH:mm:ss"), cajabo.Usuario);
+
+            return Miconexion.EjecutarComando(ComandoSQL);
+
+        }
+
+
+
+        /* public bool Eliminar()
+         {
+             return true;
+         }*/
+        public DataTable Buscar()
+        {
+            string ComandoSQL = string.Format("SELECT * FROM corte_caja");
+            return Miconexion.EjecutarSentencia(ComandoSQL);
+
+        }
+        /*  -Agregar
+           -Modificar
+           -Eliminar
+           -Buscar  */
+    }
+}
