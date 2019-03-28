83d260fed90a313995f8a0545efacb47c9dd7eee
Aaron Herrera <aaron.ssss62@gmail.com>
2019-03-27 20:10:47 -0600

    Se agrego clases DAO Y BO de Limpieza

--- Hotel/GUI/frm_programar_mantenimiento.cs
+++ Hotel/GUI/frm_programar_mantenimiento.cs
@@ -7,15 +7,31 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Hotel.BO;
+using Hotel.DAO;
 
 namespace Hotel.GUI
 {
     public partial class frm_programar_mantenimiento : Form
     {
+        LimpiezaBO limpiezaBO = new LimpiezaBO();
+        LimpiezaDAO limpiezaDAO = new LimpiezaDAO();
+        int Filaseleccionada = -1;
+        private DataTable datos;
         public frm_programar_mantenimiento()
         {
-            InitializeComponent();
+             InitializeComponent();
         }
 
+
+        private void Abrir_Limpieza(object sender, EventArgs e)
+        {
+            frm_agregar_limpieza form_agregarLimpieza = new frm_agregar_limpieza();
+            if (form_agregarLimpieza.ShowDialog() == DialogResult.OK)
+            {
+                dgv_Limpieza.DataSource = limpiezaDAO.Buscar();
+                dgv_Limpieza.Update();
+            }
+        }
     }
 }
