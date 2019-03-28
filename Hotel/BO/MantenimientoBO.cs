c9a4a311eebc3ffaae499cd523ae97a66e42edc8
Aaron Herrera <aaron.ssss62@gmail.com>
2019-03-27 20:52:29 -0600

    Creacion de clases DAO Y BO de mantenimiento

---
+++ Hotel/BO/MantenimientoBO.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Hotel.BO;
+
+namespace Hotel.BO
+{
+    class MantenimientoBO
+    {
+        private int mantenimiento_id;
+        private DateTime fecha_mantenimiento;
+        private EmpresaBO empresa;
+        private HabitacionBO habitacion;
+
+        public int Mantenimiento_id
+        {
+            get { return mantenimiento_id; }
+            set { mantenimiento_id = value; }
+        }
+
+        public DateTime Fecha_mantenimiento
+        {
+            get { return fecha_mantenimiento; }
+            set { fecha_mantenimiento = value; }
+        }
+        public EmpresaBO Empresa
+        {
+            get { return empresa; }
+            set { empresa = value; }
+        }
+        public HabitacionBO Habitacion
+        {
+            get { return habitacion; }
+            set { habitacion = value; }
+        }
+    }
+}
