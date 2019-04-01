c69dfcfabcad74c583bcba2d45616e786731c6a2
Ricardo Canul <ricardocanul7@gmail.com>
2019-03-31 19:06:28 -0600

    Se cambia labels en login para corregir mala visualizacion en Windows 7

--- Hotel/GUI/Frm_Login.Designer.cs
+++ Hotel/GUI/Frm_Login.Designer.cs
@@ -30,14 +30,14 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Frm_Login));
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.htmlLabel1 = new MetroFramework.Drawing.Html.HtmlLabel();
-            this.htmlLabel2 = new MetroFramework.Drawing.Html.HtmlLabel();
             this.txt_correo = new MetroFramework.Controls.MetroTextBox();
             this.txt_password = new MetroFramework.Controls.MetroTextBox();
             this.btn_login = new MetroFramework.Controls.MetroButton();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.pictureBox3 = new System.Windows.Forms.PictureBox();
             this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
@@ -51,30 +51,6 @@
             this.pictureBox1.Size = new System.Drawing.Size(196, 64);
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
-            // 
-            // htmlLabel1
-            // 
-            this.htmlLabel1.AutoScroll = true;
-            this.htmlLabel1.AutoScrollMinSize = new System.Drawing.Size(44, 23);
-            this.htmlLabel1.AutoSize = false;
-            this.htmlLabel1.BackColor = System.Drawing.SystemColors.Window;
-            this.htmlLabel1.Location = new System.Drawing.Point(117, 120);
-            this.htmlLabel1.Name = "htmlLabel1";
-            this.htmlLabel1.Size = new System.Drawing.Size(48, 23);
-            this.htmlLabel1.TabIndex = 1;
-            this.htmlLabel1.Text = "Correo";
-            // 
-            // htmlLabel2
-            // 
-            this.htmlLabel2.AutoScroll = true;
-            this.htmlLabel2.AutoScrollMinSize = new System.Drawing.Size(59, 23);
-            this.htmlLabel2.AutoSize = false;
-            this.htmlLabel2.BackColor = System.Drawing.SystemColors.Window;
-            this.htmlLabel2.Location = new System.Drawing.Point(103, 163);
-            this.htmlLabel2.Name = "htmlLabel2";
-            this.htmlLabel2.Size = new System.Drawing.Size(62, 23);
-            this.htmlLabel2.TabIndex = 2;
-            this.htmlLabel2.Text = "Password";
             // 
             // txt_correo
             // 
@@ -180,19 +156,39 @@
             this.linkLabel1.TabStop = true;
             this.linkLabel1.Text = "Olvide mi contraseña";
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft JhengHei UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(122, 120);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 17);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Correo:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft JhengHei UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(94, 163);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(81, 17);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Contraseña:";
+            // 
             // Frm_Login
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(544, 283);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.linkLabel1);
             this.Controls.Add(this.pictureBox3);
             this.Controls.Add(this.pictureBox2);
             this.Controls.Add(this.btn_login);
             this.Controls.Add(this.txt_password);
             this.Controls.Add(this.txt_correo);
-            this.Controls.Add(this.htmlLabel2);
-            this.Controls.Add(this.htmlLabel1);
             this.Controls.Add(this.pictureBox1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Frm_Login";
@@ -208,13 +204,13 @@
         #endregion
 
         private System.Windows.Forms.PictureBox pictureBox1;
-        private MetroFramework.Drawing.Html.HtmlLabel htmlLabel1;
-        private MetroFramework.Drawing.Html.HtmlLabel htmlLabel2;
         private MetroFramework.Controls.MetroTextBox txt_correo;
         private MetroFramework.Controls.MetroTextBox txt_password;
         private MetroFramework.Controls.MetroButton btn_login;
         private System.Windows.Forms.PictureBox pictureBox2;
         private System.Windows.Forms.PictureBox pictureBox3;
         private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
